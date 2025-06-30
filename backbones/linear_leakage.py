Filter convolution layers fixed in linear leakage model and medium simulation wiped out in linear conductive model

backbones/linear_leakage.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 from backbones.common_modules import (
-    TxaFilterEnsemble, RxaFilterEnsemble
+    TxaFilterEnsembleTorch, RxaFilterEnsembleTorch
 )
 
 
@@ -58,7 +58,7 @@ class LinearLeakage(nn.Module):
         self.out_seq_size = out_seq_size
         self.n_channels = n_channels
 
-        self.txa_filter_layers = TxaFilterEnsemble(
+        self.txa_filter_layers = TxaFilterEnsembleTorch(
             n_channels, in_seq_size, out_seq_size
         )
 
@@ -66,7 +66,7 @@ class LinearLeakage(nn.Module):
             n_channels
         )
 
-        self.rxa_filter_layers = RxaFilterEnsemble(
+        self.rxa_filter_layers = RxaFilterEnsembleTorch(
             n_channels, out_seq_size
         )
 